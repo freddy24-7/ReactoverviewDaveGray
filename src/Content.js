e5cb1668ee7570e290e2815a9df9da62a47660fd
freddy24-7 <freddy.austli@aol.com>
2022-05-16 12:23:14 +0200

    use-state

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,38 @@
 import React from 'react';
+import {useState} from "react";
 
 const Content = () => {
+
+    const [name, setName] = useState("Pesho");
+    const [count, setCount] = useState(0);
 
     const handleNameChange = (event) => {
         const names = ['John', 'Jane', 'Joe'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        setName(names[int]);
     }
+
+
     const handleClick = () => {
-        console.log('Clicked');
+        setCount(count + 1);
+        setCount(count + 1);
+        console.log(count);
 
     }
-    const handleClick2 = (name) => {
-        console.log(`${name} was clicked`);
+    const handleClick2 = () => {
+        console.log(count);
     }
-    const handleClick3 = (event) => {
-        console.log(event.target.innerText);
-    }
+
 
     return (
         <main>
-            {/*<p onDoubleClick={handleClick}>*/}
-            {/*    Hello {handleNameChange()}*/}
-            {/*</p>*/}
+            <p onDoubleClick={handleClick}>
+                Hello {name}
+            </p>
+            <button onClick={handleNameChange}>Change Name</button>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={() => handleClick2('John')}>Click Me</button>
-            <button onClick={(event) => handleClick3(event)}>Click Me</button>
+            <button onClick={handleClick2}>Click Me</button>
+
         </main>
     );
 };
