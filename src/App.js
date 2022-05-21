21d931459a3c97d763eb1148de80764ba3b8b680
freddy24-7 <freddy.austli@aol.com>
2022-05-21 17:22:56 -0400

    adding useEffect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./Header";
 import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import SearchItem from "./SearchItem";
 
 //Return function returns JSX
@@ -12,15 +12,19 @@ import SearchItem from "./SearchItem";
 
 function App() {
 
-    const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")));
+    const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")) || []);
     const [newItem, setNewItem] = useState("");
     //2 (after making search component)setting state for search
     const [search, setSearch] = useState("");
 
-    const setAndSaveItems = (newItems) => {
-        setItems(newItems);
-        localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-    };
+
+    console.log("before useEffect")
+
+    useEffect(() => {
+        localStorage.setItem("shoppinglist", JSON.stringify(items));
+    },[items]);
+
+    console.log("after usEffect")
 
     const addItem = (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -31,20 +35,19 @@ function App() {
         };
         //spread operator
         const listItems = [...items, myNewItem];
-        setAndSaveItems(listItems);
+        setItems(listItems);
 
     }
-
 
     const handleCheck = (id) => {
         const listItems = items.map((item) => item.id === id ?
             {...item, checked: !item.checked} : item);
-        setAndSaveItems(listItems);
+        setItems(listItems);
     };
 
     const handleDelete = (id) => {
         const listItems = items.filter((item) => item.id !== id);
-        setAndSaveItems(listItems);
+        setItems(listItems);
     };
 
     const handleSubmit = (event) => {
