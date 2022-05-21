9aa0ae4853d5177ff12103bbd7eddef7ce5c9870
freddy24-7 <freddy.austli@aol.com>
2022-05-21 18:50:34 -0400

    adding CRUD API calls

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Content from "./Content";
 import Footer from "./Footer";
 import {useState, useEffect} from "react";
 import SearchItem from "./SearchItem";
+import apiRequest from "./apiRequest";
 
 //Return function returns JSX
 //Title becomes a prop, rule 14
@@ -42,7 +43,7 @@ function App() {
 
     },[]);
 
-    const addItem = (item) => {
+    const addItem = async (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1;
         const myNewItem = {
             id: id,
@@ -53,17 +54,45 @@ function App() {
         const listItems = [...items, myNewItem];
         setItems(listItems);
 
+        const postOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(myNewItem)
+        };
+        const result = await apiRequest(API_URL, postOptions);
+        if (result) setFetchError (result);
     }
 
-    const handleCheck = (id) => {
+    const handleCheck = async (id) => {
         const listItems = items.map((item) => item.id === id ?
             {...item, checked: !item.checked} : item);
         setItems(listItems);
+
+        const myItem = listItems.filter((item) => item.id === id);
+        const updateOptions = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({checked: myItem[0].checked})
+        };
+        const reqUrl = `${API_URL}/${id}`;
+        const result = await apiRequest(reqUrl, updateOptions);
+        if (result) setFetchError (result);
     };
 
-    const handleDelete = (id) => {
+    const handleDelete = async (id) => {
         const listItems = items.filter((item) => item.id !== id);
         setItems(listItems);
+
+        const deleteOptions = {
+            method: "DELETE",
+        };
+        const reqUrl = `${API_URL}/${id}`;
+        const result = await apiRequest(reqUrl, deleteOptions);
+        if (result) setFetchError (result);
     };
 
     const handleSubmit = (event) => {
@@ -71,6 +100,7 @@ function App() {
         if (!newItem) return;
         addItem(newItem);
         setNewItem("");
+
     };
 
     //3) Below add search item
